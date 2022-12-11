edd3cb6e5843b3d9e03514973b6c2a3211092048
nely de faria <nelly.nf54@gmail.com>
2022-12-11 11:31:38 -0300

    Cria comentarios.php

---
+++ php-mastery/comentarios.php
@@ -0,0 +1,11 @@
+<?php
+
+# Este é um comentário de linha.
+// Outro comentário de linha.
+
+/*
+    Este é um comentário de várias linhas.
+    Outra linha de comentário.
+*/
+
+echo 'Comments in PHP';
