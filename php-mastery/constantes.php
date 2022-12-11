c22be69a972c257c45e350371cb374f32650a3a7
nely de faria <nelly.nf54@gmail.com>
2022-12-11 11:35:29 -0300

    Cria constantes.php

---
+++ php-mastery/constantes.php
@@ -0,0 +1,17 @@
+<?php
+
+// Definindo constante
+define('LINK_SITE', 'http://codeexperts.com.br');
+echo LINK_SITE;
+echo '<br>';
+
+echo 'Constante PHP versão: ' . PHP_VERSION;
+echo '<br>';
+
+echo 'Meu sistema é ' . PHP_OS;
+echo '<br>';
+
+echo 'Estamos na pasta: ' . __DIR__;
+echo '<br>';
+
+echo 'Arquivo: ' . __FILE__;
