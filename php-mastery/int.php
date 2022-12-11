be1653df7d7657b8d66362b67baccd5f3c4d8fe5
nely de faria <nelly.nf54@gmail.com>
2022-12-11 11:32:41 -0300

    Cria int.php

---
+++ php-mastery/int.php
@@ -0,0 +1,18 @@
+<?php
+
+// Inteiros 1,2,3,4,5,6,7,8,9 -1, -2
+var_dump(-2);
+
+// Sistema octal no PHP
+// echo 0777; 
+var_dump(0777);
+
+// Sistema hexadecimal
+var_dump(0x1FF);
+
+// Sistema binário
+var_dump(0b100);
+
+echo PHP_INT_MIN;
+echo '<br>';
+echo PHP_INT_MAX;
