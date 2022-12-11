442ab3f35db46d66ca412c2483b7b9e2ff17a900
nely de faria <nelly.nf54@gmail.com>
2022-12-11 11:32:58 -0300

    Cria float.php

---
+++ php-mastery/float.php
@@ -0,0 +1,22 @@
+<?php
+
+// Float
+var_dump(10.99);
+
+var_dump(0.90);
+
+// echo gettype(10.90);
+
+echo PHP_FLOAT_MIN;
+
+echo '<br>';
+
+echo PHP_FLOAT_MAX;
+
+echo '<br>';
+
+echo 10e+2; // 1000
+
+echo '<br>';
+
+echo 14e-2;
