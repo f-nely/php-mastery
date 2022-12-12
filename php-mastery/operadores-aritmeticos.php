1abeffadcebe58ddebd24baaf7c07379c7b28d05
nely de faria <nelly.nf54@gmail.com>
2022-12-12 16:22:35 -0300

    Cria operadores-aritmeticos.php

---
+++ php-mastery/operadores-aritmeticos.php
@@ -0,0 +1,11 @@
+<?php
+
+$num1 = 10;
+$num2 = 20;
+
+echo "$num1 + $num2 = " . $num1 + $num2 . "<br>";
+echo "$num1 - $num2 = " . $num1 - $num2 . "<br>";
+echo "$num1 * $num2 = " . $num1 * $num2 . "<br>";
+echo "$num1 / $num2 = " . $num1 / $num2 . "<br>";
+echo "$num1 % $num2 = " . $num1 % $num2 . "<br>";
+echo "3 ** 4 = " . 3 ** 4; // 3 * 3 * 3 * 3 = 81
