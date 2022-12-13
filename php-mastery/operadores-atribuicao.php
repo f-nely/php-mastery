00aed09927a92129e869233f7e61504ffc2eadd7
nely de faria <nelly.nf54@gmail.com>
2022-12-13 19:24:35 -0300

    Cria operadores-atribuicao.php

---
+++ php-mastery/operadores-atribuicao.php
@@ -0,0 +1,24 @@
+<?php
+
+$num = 10;
+
+$num += 5; // $num = $num + 5;
+
+var_dump($num);
+
+$num -= 1;
+
+var_dump($num);
+
+$num *= 2;
+
+var_dump($num);
+
+$num /= 4;
+
+var_dump($num);
+
+$name = 'Harry';
+$name .= ' Kane';
+
+var_dump($name);
