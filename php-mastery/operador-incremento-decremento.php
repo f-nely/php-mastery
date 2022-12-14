c4f323921af2b83d2277805413527c41ded674c0
nely de faria <nelly.nf54@gmail.com>
2022-12-14 20:19:31 -0300

    Cria operador-incremento-decremento.php

---
+++ php-mastery/operador-incremento-decremento.php
@@ -0,0 +1,20 @@
+<?php
+
+$num = 1;
+
+// Pós incremento
+$num++;
+echo $num;
+echo '<br>';
+
+// Pré incremento
+echo ++$num;
+echo '<br>';
+
+// Pós decremento
+$num--;
+echo $num;
+echo '<br>';
+
+// Pré decremento
+echo --$num;
