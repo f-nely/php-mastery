46fc41f5cf6b569881dc466a05f711f415c58342
nely de faria <nelly.nf54@gmail.com>
2022-12-15 11:18:36 -0300

    Cria if-elseif-else.php

---
+++ php-mastery/if-elseif-else.php
@@ -0,0 +1,11 @@
+<?php
+
+$countStr = strlen('Giroud');
+
+if ($countStr == 5) {
+    echo 'String possui 5 caracteres...';
+} else if ($countStr == 6) {
+    echo 'String possui 6 caracteres...';
+} else {
+    echo 'A string não possui cinco caracteres...';
+}
