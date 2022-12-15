62c625ef2619655043c9be455c6e9a5764749d1f
nely de faria <nelly.nf54@gmail.com>
2022-12-15 11:18:53 -0300

    Cria switch.php

---
+++ php-mastery/switch.php
@@ -0,0 +1,17 @@
+<?php
+
+$fruit = 'strawberry';
+
+switch ($fruit) {
+    case 'apple':
+        echo 'The fruit is apple';
+        break;
+    case 'orange':
+        echo 'The fruit is orange';
+        break;
+    case 'grapes':
+        echo 'The fruit is grape';
+        break;
+    default:
+        echo 'Invalid input';
+}
