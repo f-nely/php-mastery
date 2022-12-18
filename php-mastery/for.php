f34412e78e9bfb1b517a233ba4dbd0b599489403
nely de faria <nelly.nf54@gmail.com>
2022-12-18 11:45:16 -0300

    Cria for.php

---
+++ php-mastery/for.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($i = 10; $i >= 0; $i--) {
+    echo $i . '<br>';
+}
+
+$cars = ['Cronos', 'Amarok', 'KWID', 'Onix', 'T-cross'];
+
+for ($i = 0; $i < count($cars); $i++) {
+    echo $cars[$i] . '<br>';
+}
