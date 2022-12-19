6825c7970a1584ee0a540e4eea851092a5e6f2ad
nely de faria <nelly.nf54@gmail.com>
2022-12-19 19:01:36 -0300

    Cria foreach.php

---
+++ php-mastery/foreach.php
@@ -0,0 +1,13 @@
+<?php
+
+$cars = ['Renegade', 'S10', 'Honda Civic', 'Jeep Compass'];
+
+foreach ($cars as $car) {
+    echo $car . '<br>';
+}
+
+$numbers = range(0, 10);
+
+foreach ($numbers as $number) {
+    echo $number . '<br>';
+}
