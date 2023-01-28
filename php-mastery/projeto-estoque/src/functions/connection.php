04ff1246924fc88ca1b74d21069e83e4864acfca
nely de faria <nelly.nf54@gmail.com>
2023-01-28 18:53:00 -0300

    Inserindo dados via PDO

--- php-mastery/projeto-estoque/src/functions/connection.php
+++ php-mastery/projeto-estoque/src/functions/connection.php
@@ -2,5 +2,13 @@
 
 $conn = new PDO('mysql:dbname=controle_estoque;host=mysql', 'root', '<root-password>');
 
-var_dump($conn);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// var_dump($conn);
+
+// insert
+$sql = "INSERT INTO produtos(nome, descricao, sobre, preco, status, criado_em, atualizado_em)
+VALUES('Produto via PHP', 'Descrição via PHP', 'Sobre via PHP', 255.96, 1, NOW(), NOW())";
+
+var_dump($conn->query($sql));
 exit;
